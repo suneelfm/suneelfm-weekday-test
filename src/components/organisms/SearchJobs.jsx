d8a3d8cd25c8e7c4d8a1720a28ed6175c94487cc
suneelfm <sfm.20031998@gmail.com>
2024-05-05 14:23:20 +0530

    Designed the UI of search job page with sample data

--- src/components/organisms/SearchJobs.jsx
+++ src/components/organisms/SearchJobs.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Grid } from "@mui/material";
+import FilterBar from "../molecules/FilterBar";
+import JobCard from "../molecules/JobCard";
+import { useSelector } from "react-redux";
 
 export default function SearchJobs() {
-  return <div>SearchJobs</div>;
+  const filters = useSelector((state) => state.filterJobs);
+  useEffect(() => {
+    console.log(filters);
+  }, [filters]);
+
+  return (
+    <Grid container my={1}>
+      <FilterBar />
+      <Grid container p={3} spacing={4}>
+        {[1, 2, 3, 4, 5, 6, 6].map((job, i) => (
+          <Grid key={`job-${i + 1}`} item xs={12} sm={6} md={4} lg={3}>
+            <JobCard />
+          </Grid>
+        ))}
+      </Grid>
+    </Grid>
+  );
 }
